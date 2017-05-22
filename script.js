eeb461ee0b069849f39a2624386926816aaae54d
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-05-22 15:15:00 +0200

    ajout d'animation sur les pulsegun de Tracer

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-    
-let Tracer = {
-    hp : 150
-};
+    let Tracer = {
+        hp: 150
+    };
 
-let Pharah = {
-    hp : 200
-};
+    let Pharah = {
+        hp: 200
+    };
 
-let pulsegun = document.querySelector("#gun").addEventListener("click", function(){
-                Pharah.hp -= 20;
-                console.log(Pharah.hp);
-                document.querySelector("#pharahhp").textContent = Pharah.hp;
-})
+    document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+    document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
 
-let rocketlauncher = document.querySelector("#rocket").addEventListener("click", function(){
-                Tracer.hp -= 15;
-                console.log(Tracer.hp);
-                document.querySelector("#tracerhp").textContent = Tracer.hp;
-})
+    let pulsegun = document.querySelector("#gun").addEventListener("click", function() {
+        Pharah.hp -= 20;
+        console.log(Pharah.hp);
+        document.querySelector('.traceranim').style.animation = "tracerpulsegun 1s steps(1)";
+        setTimeout(function() {
+            document.querySelector('#tirs').style.display = 'block';
+        }, 410);
+        setTimeout(function() {
+            document.querySelector('#tirs').style.display = 'none';
+        }, 1000)
+        document.querySelector('.traceranim').addEventListener("animationend", function() {
+            document.querySelector('.traceranim').style.animationName = "";
+        })
+        document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
+    })
 
-document.querySelector("#jouer").addEventListener("click",function(){
-    document.querySelector("#menu").style.display = 'none';
-})
 
-document.querySelector("#tracerhp").textContent = Tracer.hp;
-document.querySelector("#pharahhp").textContent = Pharah.hp;
+    let rocketlauncher = document.querySelector("#rocket").addEventListener("click", function() {
+        Tracer.hp -= 15;
+        console.log(Tracer.hp);
+        document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+    })
 
+    document.querySelector("#jouer").addEventListener("click", function() {
+        document.querySelector("#menu").style.display = 'none';
+    })
