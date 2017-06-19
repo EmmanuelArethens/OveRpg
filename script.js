e7c26c5af71267d885ea07b610c4e7146689630a
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-06-19 15:38:43 +0200

    ajout d'une screenshot et modfi fonction

--- script.js
+++ script.js
@@ -1,84 +1,95 @@
-    let Tracer = {
-        hp: 150,
-        ult: 0,
-    };
+let Tracer = {
+    hp: 150,
+    ult: 0,
+};
 
-    let Pharah = {
-        hp: 200,
-        ult: 0,
-    };
+let Pharah = {
+    hp: 200,
+    ult: 0,
+};
 
-    document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+
+
+function TracerBomb() {};
+
+function TracerRecall() {};
+
+function TracerBlinkattack() {};
+
+function PharahRocketlauncher() {};
+
+function PharahConcussiveblast() {};
+
+function PharahJump() {};
+
+function PharahRocketbarrage() {};
+
+function TracerPulsegun(attaquant, defenseur) {
+    console.log('test')
+    defenseur.hp -= 20;
+    attaquant.ult += 10;
+    console.log(attaquant.ult);
+    console.log(defenseur.hp);
+    document.querySelector('.traceranim').style.animation = "tracerpulsegun 1s steps(1)";
+    setTimeout(function() {
+        document.querySelector('#tirs').style.display = 'block';
+    }, 410);
+    setTimeout(function() {
+        document.querySelector('#tirs').style.display = 'none';
+    }, 1000)
+    document.querySelector('.traceranim').addEventListener("animationend", function() {
+        document.querySelector('.traceranim').style.animationName = "";
+    })
+};
+
+document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
+document.querySelector("#tracerult").textContent = Tracer.ult + "%";
+document.querySelector("#pharahult").textContent = Pharah.ult + "%";
+
+document.querySelector("#gun").addEventListener("click", function() {
+    TracerPulsegun(Tracer, Pharah);
+});
+
+document.querySelector("#bomb").addEventListener("click", function() {
+    Pharah.hp -= 100;
+    console.log(Pharah.hp);
     document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
-    document.querySelector("#tracerult").textContent = Tracer.ult + "%";
-    document.querySelector("#pharahult").textContent = Pharah.ult + "%";
-
-    let pulsegun = document.querySelector("#gun").addEventListener("click", function() {
-        Pharah.hp -= 20;
-        Tracer.ult += 10;
-        console.log(Tracer.ult);
-        console.log(Pharah.hp);
-        document.querySelector('.traceranim').style.animation = "tracerpulsegun 1s steps(1)";
-        setTimeout(function() {
-            document.querySelector('#tirs').style.display = 'block';
-        }, 410);
-        setTimeout(function() {
-            document.querySelector('#tirs').style.display = 'none';
-        }, 1000)
-        document.querySelector('.traceranim').addEventListener("animationend", function() {
-            document.querySelector('.traceranim').style.animationName = "";
-        })
-        document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
-        document.querySelector("#tracerult").textContent = Tracer.ult + "%";
-        if (Pharah.hp <= 0) {
-            alert('Tracer win');
-        }
-
-    })
-
-    let bomb = document.querySelector("#bomb").addEventListener("click", function() {
-        Pharah.hp -= 100;
-        console.log(Pharah.hp);
-        document.querySelector("#pharahhp").textContent = Pharah.hp + " /200 hp";
-        if (Pharah.hp <= 0) {
-            alert('Tracer win');
-        }
-    })
-
-
-    let rocketlauncher = document.querySelector("#rocket").addEventListener("click", function() {
-        Tracer.hp -= 30;
-        console.log(Tracer.hp);
-        document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
-        if (Tracer.hp <= 0) {
-            alert('Pharah win');
-        };
-    })
-
-    let rocketbarrage = document.querySelector("#barrage").addEventListener("click", function() {
-        Tracer.hp -= 150;
-        console.log(Tracer.hp);
-        document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
-        if (Tracer.hp <= 0) {
-            alert('Pharah win');
-            Tracer.hp = 0;
-        };
-    })
-
-
-    let recall = document.querySelector("#recall").addEventListener("click", function() {
-        Tracer.hp += 30;
-        console.log(Tracer.hp);
-        document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
-        if (Tracer.hp >= 130) {
-            Tracer.hp = 120;
-        }
-    })
-
-    document.querySelector("#jouer").addEventListener("click", function() {
-        document.querySelector("#menu").style.display = 'none';
-    })
-
-    if (Tracer.ult > 99) {
-
+    if (Pharah.hp <= 0) {
+        alert('Tracer win');
     }
+})
+
+
+document.querySelector("#rocket").addEventListener("click", function() {
+    Tracer.hp -= 30;
+    console.log(Tracer.hp);
+    document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+    if (Tracer.hp <= 0) {
+        alert('Pharah win');
+    };
+})
+
+document.querySelector("#barrage").addEventListener("click", function() {
+    Tracer.hp -= 150;
+    console.log(Tracer.hp);
+    document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+    if (Tracer.hp <= 0) {
+        alert('Pharah win');
+        Tracer.hp = 0;
+    };
+})
+
+
+document.querySelector("#recall").addEventListener("click", function() {
+    Tracer.hp += 30;
+    console.log(Tracer.hp);
+    document.querySelector("#tracerhp").textContent = Tracer.hp + " /150 hp";
+    if (Tracer.hp >= 130) {
+        Tracer.hp = 120;
+    }
+})
+
+document.querySelector("#jouer").addEventListener("click", function() {
+    document.querySelector("#menu").style.display = 'none';
+})
